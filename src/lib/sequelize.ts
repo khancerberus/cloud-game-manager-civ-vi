6d6a0d92f2b03172e9880154f3afdb412a64b58b
Victor Valenzuela <vi.valenzuelam@gmail.com>
2026-05-31 18:44:12 -0400

    Synchronize models after database connection
    Added synchronization of models after successful authentication.

--- src/lib/sequelize.ts
+++ src/lib/sequelize.ts
@@ -11,6 +11,9 @@ export const sequelize = new Sequelize(DATABASE_URL, {
 try {
     sequelize.authenticate()
     console.log('Connection has been established successfully.')
+
+    await sequelize.sync() 
+    console.log('Todos los modelos se han sincronizado con éxito.')
 } catch (error) {
     console.error('Unable to connect to the database:', error)
 }
